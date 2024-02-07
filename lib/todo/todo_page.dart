213ef7cece32400a349c3dad543d7e9c7f362dd1
hapham hapham <hapham@bbossystem.com>
2024-02-07 22:00:28 +0700

    add _ToDoListState

--- lib/todo/todo_page.dart
+++ lib/todo/todo_page.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'todo_header.dart';
+import 'todo_list.dart';
 
 class ToDoListPage extends StatelessWidget {
   const ToDoListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const ToDoHeader();
+    return const Column( 
+      children: [
+        ToDoHeader(),
+        Expanded(child: ToDoList()),
+      ],
+    ); 
   }
 
 }
